50df4d801171b594b9bd31f6ec64ec2069941937
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-01 12:31:05 +0000

    finished

--- main.cpp
+++ main.cpp
@@ -19,5 +19,47 @@ int main() {
 	cin >> palavra;
 	system("cls");
 	
+	while(palavra[i] != '\0') {
+		i++;
+		tam++;
+	}
+	
+	for (i=0; i<30; i++) {
+		secreta[i] = '-';
+	}
+	
+	while((chances > 0) && (acertos < tam)) { 
+		cout << "Chances restantes: " << chances << "\n\n";
+		cout << "Palavra secreta: ";
+		for (i=0; i < tam; i++) {
+			cout << secreta[i];
+		}
+		
+		cout << "\n\nDigite uma letra: ";
+		cin >> letra[0];
+		
+		for (i=0; i<tam; i++) {
+			if(palavra[i] == letra[0]) {
+				acerto = true;
+				secreta[i] = palavra[i];
+				acertos++;
+			}
+		}
+		
+		if (!acertos) {
+			chances--;	
+		}
+		acerto = false;
+		system("cls");
+	}
+	
+	if (acertos == tam){
+		cout << "Voce ganhou!!";
+	} else {
+		cout << "Que pena, voce perdeu!!";
+	}
+	
+	system("pause");
 	return 0;
+	
 }
