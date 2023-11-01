e9d128fb67f6a1d2844eeff44bd31dbe8834c95b
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-01 12:10:11 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+# include <stdlib.h>
+
+using namespace std;
+
+int main() {
+	
+	char palavra[30], letra[1], secreta[30];
+	int tam, i, chances, acertos;
+	bool acerto=false;
+	
+	chances = 6;
+	tam = 0;
+	i = 0;
+	acerto = false;
+	acertos = 0;
+	
+	cout << "Fale para seu amigo fechar o olho e digite a palavra secreta: ";
+	cin >> palavra;
+	system("cls");
+	
+	return 0;
+}
